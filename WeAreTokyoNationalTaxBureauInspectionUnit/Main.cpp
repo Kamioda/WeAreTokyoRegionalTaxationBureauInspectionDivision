a33c177f3229362e0e3873885bcc32e738edef66
AinoMegumi <aimegu2014@gmail.com>
2017-10-22 15:49:43 +0900

    再定義となっていた部分を削除 ServicePackの完成の目処が立たないため、エラーログの送信部分を削除

--- WeAreTokyoNationalTaxBureauInspectionUnit/Main.cpp
+++ WeAreTokyoNationalTaxBureauInspectionUnit/Main.cpp
@@ -1,38 +1,6 @@
-#include "Send.hpp"
 #include "D:/Users/AinoMegumi/BitBucket/Kamioda Games/CommonSourceCode/Split.hpp"
 #include <Windows.h>
 #include "KgWinException.hpp"
-
-static inline void SendExceptionMessageToService(const std::string GameTitle, const std::string ExceptionName, const std::string ExceptionMessage) {
-	try {
-		std::ostringstream ostr;
-		Send snd(ostr, 300); // KgSvchost.exeの例外の出力スレッドに渡す
-		snd.InputData(GameTitle);
-		snd.InputData(ExceptionName);
-		snd.InputData(ExceptionMessage);
-		snd.send(ostr);
-	}
-	catch (const std::exception& er) {
-		MessageBoxA(NULL, 
-			(std::string("サービスへのエラーメッセージの送信に失敗しました。\n原因 : ") + er.what()).c_str(),
-			"東京国税局査察部です", MB_ICONERROR | MB_OK);
-	}
-}
-
-std::vector<std::string> SplitString(const std::string Data, const std::string sep) {
-	std::vector<std::string> Arr;
-	size_t pos = 0;
-	while (pos != std::string::npos) {
-		size_t p = Data.find(sep, pos);
-		if (p == std::string::npos) {
-			Arr.push_back(Data.substr(pos));
-			break;
-		}
-		else Arr.push_back(Data.substr(pos, p - pos));
-		pos = p + sep.size();
-	}
-	return Arr;
-}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow) {
 	UNREFERENCED_PARAMETER(hInstance);
@@ -43,15 +11,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		}
 		else {
-			const std::vector<std::string> Args = SplitString(lpCmdLine, " ");
+			const std::vector<std::string> Args = SplitA(lpCmdLine, ' ');
 		}
 	}
 	catch (const std::exception& er) {
-		SendExceptionMessageToService("WeAreTokyoNationalTaxBureauInspectionUnit", "std::exception", er.what());
 		return MessageBoxA(NULL, er.what(), "東京国税局査察部です", MB_ICONERROR | MB_OK);
 	}
 	catch (const KgWinException& kex) {
-		SendExceptionMessageToService("WeAreTokyoNationalTaxBureauInspectionUnit", "KgWinException", kex.what());
 		return kex.GraphErrorMessageOnMessageBox("東京国税局査察部です");
 	}
 }
