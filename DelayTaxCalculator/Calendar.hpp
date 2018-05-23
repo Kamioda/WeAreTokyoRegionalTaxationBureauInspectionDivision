84756ba02769c42019d7dd5476ce65e9821053d9
AinoMegumi <aimegu2014@gmail.com>
2018-05-23 20:04:51 +0900

    延滞税計算機を作成開始

---
+++ DelayTaxCalculator/Calendar.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <ctime>
+#include <functional>
+
+class Calendar {
+public:
+	Calendar();
+	Calendar(const int Year, const int Month, const int Day);
+	Calendar GetAfterTwoMonthDay() const;
+	Calendar GetAfterAWeekDay() const;
+	Calendar operator - (const Calendar cal) const;
+	time_t to_time_t() const;
+	int Year, Month, Day;
+};
